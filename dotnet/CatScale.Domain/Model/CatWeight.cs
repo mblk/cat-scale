ea0fac8d3fcc8e8ca03d23e0f4ecb93f2e010bb7
Marcel Block <marcelblock@gmail.com>
2023-05-02 07:44:36 +0200

    Fix incorrect access modifier in model class.

--- dotnet/CatScale.Domain/Model/CatWeight.cs
+++ dotnet/CatScale.Domain/Model/CatWeight.cs
@@ -4,7 +4,7 @@ public class CatWeight
 {
     public int Id { get; set; }
     
-    private int CatId { get; set; }
+    public int CatId { get; set; }
     //public Cat Cat { get; set; } = null!;
     
     public DateTimeOffset Timestamp { get; set; }

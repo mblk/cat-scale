f81c409796c591f63ace3970af6eed3d767e39b7
mblk <MarcelBlock@gmail.com>
2023-08-05 09:53:53 +0200

    Improved logic in filter-cascade and cleanup.

--- esp32/cat_scale/main/filter_cascade.c
+++ esp32/cat_scale/main/filter_cascade.c
@@ -4,89 +4,145 @@
 #include "filters.h"
 #include "measurement.h"
 
-#include "sdkconfig.h"
-
-#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
 #include <assert.h>
 
-static high_pass_filter_t *g_hpf1 = NULL;
-static low_pass_filter_t *g_lpf1 = NULL;
-static mean_filter_t *g_mean1 = NULL;
-static median_filter_t *g_median1 = NULL;
-static differentiator_t *g_dxdt1 = NULL;
+static high_pass_filter_t *g_hpf = NULL;
+static low_pass_filter_t *g_lpf = NULL;
+static mean_filter_t *g_mean = NULL;
+static median_filter_t *g_median = NULL;
+static differentiator_t *g_dxdt = NULL;
+
+#if DEBUG_FILTER_CASCADE
+static double g_first_input_value = 0.0;
+#endif
+
+#define HPF_HISTORY_SIZE    (10)
+#define STABLE_VALUES_SIZE  (1000)
 
 static bool g_input_switch = false;
-static double g_input_offset = 0;
-static double g_prev_hpf1_offsets[10] = {};
-static double g_input_switch_timer = 0;
+static double g_input_offset = 0.0;
+static double g_prev_hpf_offsets[HPF_HISTORY_SIZE] = {};
+static double g_input_switch_timer = 0.0;
 
 static double g_stable_time = 0.0;
-static double g_stable_phase_values[1000] = {}; // TODO
-static int g_stable_phase_values_index = 0;
+static double g_stable_phase_values[STABLE_VALUES_SIZE] = {};
+static int g_stable_phase_values_count = 0;
 
-static const double sampling_frequency = 10.0;
-static const double dxdt_threshold = 50.0;
+static const double cfg_sampling_frequency = 10.0;
+static const double cfg_dxdt_threshold = 50.0;
+static const double cfg_stable_phase_min_time = 2.5;
+static const double cfg_hold_timer = 10.0;
+static const double cfg_hold_timeout = 300.0;
+static const double cfg_hold_weight_low = -500.0;
+static const double cfg_hold_weight_high = 5000.0;
+static const double cfg_calibration_factor = 1.0 / 23.0;
 
 void filter_cascade_init(void)
 {
-    g_hpf1 = create_high_pass_filter(sampling_frequency, 0.1);
-    g_lpf1 = create_low_pass_filter(sampling_frequency, 0.5);
-    g_mean1 = create_mean_filter(10);
-    g_median1 = create_median_filter(10);
-    g_dxdt1 = create_differentiator(sampling_frequency);
+    g_hpf = create_high_pass_filter(cfg_sampling_frequency, 0.1);
+    g_lpf = create_low_pass_filter(cfg_sampling_frequency, 0.5);
+    g_mean = create_mean_filter(10);
+    g_median = create_median_filter(10);
+    g_dxdt = create_differentiator(cfg_sampling_frequency);
+
+#if DEBUG_FILTER_CASCADE
+    g_first_input_value = 0.0;
+#endif
+
+    g_input_switch = false;
+    g_input_offset = 0.0;
+    memset(g_prev_hpf_offsets, 0, sizeof(g_prev_hpf_offsets));
+    g_input_switch_timer = 0.0;
+
+    g_stable_time = 0.0;
+    memset(g_stable_phase_values, 0, sizeof(g_stable_phase_values));
+    g_stable_phase_values_count = 0;
+}
+
+void filter_cascade_cleanup(void)
+{
+    if (g_hpf) destroy_high_pass_filter(g_hpf);
+    if (g_lpf) destroy_low_pass_filter(g_lpf);
+    if (g_mean) destroy_mean_filter(g_mean);
+    if (g_median) destroy_median_filter(g_median);
+    if (g_dxdt) destroy_differentiator(g_dxdt);
+
+    g_hpf = NULL;
+    g_lpf = NULL;
+    g_mean = NULL;
+    g_median = NULL;
+    g_dxdt = NULL;
 }
 
 static void push_stable_value(double value, double dt)
 {
     g_stable_time += dt;
-    g_stable_phase_values[g_stable_phase_values_index % 1000] = value;
-    g_stable_phase_values_index++;
+
+    if (g_stable_phase_values_count < STABLE_VALUES_SIZE)
+        g_stable_phase_values[g_stable_phase_values_count++] = value;
 }
 
 static void clear_stable_phase(void)
 {
-    if (g_stable_time >= 2.0)
+    if (g_stable_time >= cfg_stable_phase_min_time &&
+        g_stable_phase_values_count > 0 &&
+        g_input_switch)
     {
-        int num_values = g_stable_phase_values_index;
-        if (g_stable_phase_values_index >= 1000) num_values = 1000;
-
         double avg = 0;
-        for(int i=0; i<num_values; i++)
+        for(size_t i=0; i<g_stable_phase_values_count; i++)
             avg += g_stable_phase_values[i];
-        avg /= (double)num_values;
+        avg /= (double)g_stable_phase_values_count;
 
-        //printf("[%u] End of stable phase (%0.1f s, %0.1f avg, %d values)\n", process_value_index, g_stable_time, avg, num_values);
         measurement_push_stable_phase(g_stable_time, avg);
     }
 
     g_stable_time = 0.0;
-    memset(g_stable_phase_values, 0, sizeof(double) * 1000);
-    g_stable_phase_values_index = 0;
+    memset(g_stable_phase_values, 0, sizeof(g_stable_phase_values));
+    g_stable_phase_values_count = 0;
 }
 
 double filter_cascade_process(double input, double dt)
 {
-    assert(g_hpf1);
+    assert(g_hpf);
 
-    double output_hpf1 = high_pass_filter(g_hpf1, input);
+#if DEBUG_FILTER_CASCADE
+    if (g_first_input_value == 0.0)
+        g_first_input_value = input;
+#endif
 
-    for(int i=9; i>0; i--) g_prev_hpf1_offsets[i] = g_prev_hpf1_offsets[i-1];
-    g_prev_hpf1_offsets[0] = output_hpf1 - input;
+    const double output_hpf1 = high_pass_filter(g_hpf, input);
+    const double input_for_lpf = g_input_switch ? (input + g_input_offset) : output_hpf1;
+    const double output_lpf = low_pass_filter(g_lpf, input_for_lpf);
+    const double output_mean = mean_filter(g_mean, output_lpf);
+    const double output_median = median_filter(g_median, output_mean);
+    const double output_grams = output_median * cfg_calibration_factor;
+    const double output_dxdt = differentiate(g_dxdt, output_grams);
 
-    double input_for_lp1 = g_input_switch ? (input + g_input_offset) : output_hpf1;
-    double output_lpf1 = low_pass_filter(g_lpf1, input_for_lp1);
-    double output_mean1 = mean_filter(g_mean1, output_lpf1);
-    double output_median1 = median_filter(g_median1, output_mean1);
+    for(size_t i=HPF_HISTORY_SIZE-1; i>0; i--) g_prev_hpf_offsets[i] = g_prev_hpf_offsets[i-1];
+    g_prev_hpf_offsets[0] = output_hpf1 - input;
 
-    double output_grams = output_median1 / 23.0; // TODO calibration
+#if DEBUG_FILTER_CASCADE
+    filter_cascade_debug("input", input - g_first_input_value + 50000);
+    filter_cascade_debug("hpf hold", g_input_switch ? (100000) : (75000));
+    //filter_cascade_debug("hpf out", output_hpf);
+    //filter_cascade_debug("lpf in", input_for_lpf);
+    //filter_cascade_debug("lpf out", output_lpf);
+    //filter_cascade_debug("mean out", output_mean);
+    filter_cascade_debug("median out", output_median);
+    //filter_cascade_debug("grams out", output_grams);
+    //filter_cascade_debug("dxdt out", output_dxdt);
+#endif
 
-    double output_dxdt1 = differentiate(g_dxdt1, output_grams);
+    const bool signal_stable = fabs(output_dxdt) < cfg_dxdt_threshold;
 
+    const bool hold_trigger = !signal_stable ||
+        output_grams < cfg_hold_weight_low ||
+        output_grams > cfg_hold_weight_high;
 
-    if (fabs(output_dxdt1) < dxdt_threshold) // TODO use stddev instead?
+    if (signal_stable)
     {
         push_stable_value(output_grams, dt);
     }
@@ -95,32 +151,36 @@ double filter_cascade_process(double input, double dt)
         clear_stable_phase();
     }
 
-    // activate hpf-hold-switch?
-    if (fabs(output_dxdt1) > dxdt_threshold || output_grams > 1000.0 || output_grams < -400.0) // TODO does not work when refilling
+    if (hold_trigger)
     {
+        // activate switch?
         if (!g_input_switch)
         {
             measurement_mark_start_of_event();
             g_input_switch = true;
-            g_input_offset = g_prev_hpf1_offsets[9]; // TODO mean/median?
+            g_input_offset = g_prev_hpf_offsets[HPF_HISTORY_SIZE-1];
         }
 
-        g_input_switch_timer = 5.0; // must not deactivate for x seconds
+        g_input_switch_timer = cfg_hold_timer;
     }
 
-    // deactivate switch?
     if (g_input_switch)
     {
-        g_input_switch_timer -= dt; // dt
-        if (g_input_switch_timer <= 0)
+        g_input_switch_timer -= dt;
+
+        // deactivate switch?
+        if (g_input_switch_timer <= 0 || g_stable_time >= cfg_hold_timeout)
         {
+            clear_stable_phase();
             measurement_mark_end_of_event();
+
             g_input_switch = false;
-            g_hpf1->reset = true;
-            g_lpf1->reset = true;
-            g_mean1->reset = true;
-            g_median1->reset = true;
-            g_dxdt1->reset = true;
+
+            g_hpf->reset = true;
+            g_lpf->reset = true;
+            g_mean->reset = true;
+            g_median->reset = true;
+            g_dxdt->reset = true;
         }
     }
 
--- esp32/cat_scale/main/filter_cascade.h
+++ esp32/cat_scale/main/filter_cascade.h
@@ -1,5 +1,10 @@
 #pragma once
 
 void filter_cascade_init(void);
+void filter_cascade_cleanup(void);
 
 double filter_cascade_process(double input, double dt);
+
+#if DEBUG_FILTER_CASCADE
+void filter_cascade_debug(const char *id, double value);
+#endif

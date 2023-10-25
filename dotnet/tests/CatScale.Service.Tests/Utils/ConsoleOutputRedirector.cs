a409fc1124ffd716b8bd0f2469e5c5b59c451efd
Marcel Block <MarcelBlock@gmail.com>
2023-10-25 13:23:21 +0200

    Prevent using the ITestOutputHelper after the test has finished.

--- dotnet/tests/CatScale.Service.Tests/Utils/ConsoleOutputRedirector.cs
+++ dotnet/tests/CatScale.Service.Tests/Utils/ConsoleOutputRedirector.cs
@@ -1,13 +1,12 @@
 using System.Text;
-using Xunit.Abstractions;
 
 namespace CatScale.Service.Tests.Utils;
 
-internal class ConsoleOutputRedirector : TextWriter
+internal class ConsoleOutputRedirector : TextWriter, IDisposable
 {
     private readonly ITestOutputHelper _output;
-
     private readonly StringBuilder _buffer = new();
+    private bool _disposed;
     
     public override Encoding Encoding => Encoding.UTF8;
     
@@ -15,19 +14,28 @@ internal class ConsoleOutputRedirector : TextWriter
     {
         _output = output;
     }
-    
+
+    protected override void Dispose(bool disposing)
+    {
+        _disposed = true; // Must no longer use the ITestOutputHelper
+        base.Dispose(disposing);
+    }
+
     public override void WriteLine(string? message)
     {
+        if (_disposed) return;
         _output.WriteLine(message);
     }
     
     public override void WriteLine(string format, params object?[] args)
     {
+        if (_disposed) return;
         _output.WriteLine(format, args);
     }
 
     public override void Write(char value)
     {
+        if (_disposed) return;
         if (value is '\n' or '\r')
         {
             if (_buffer.Length > 0)
